fc5e4ded6ab75ede49ce2f860ff1b124cdf45a59
ABaulderstone <alex.baulderstone@gmail.com>
2026-02-27 16:49:03 +1030

    adds params to session controller

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -8,9 +8,9 @@ class SessionsController < ApplicationController
 
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: session_params[:email])
 
-    if user&.authenticate(params[:password])
+    if user&.authenticate(session_params[:password])
       token = JwtService.encode(user_id: user.id)
 
       cookies.signed[:jwt] = {
@@ -29,4 +29,9 @@ class SessionsController < ApplicationController
     cookies.delete(:jwt)
     head :no_content
   end
+
+  private
+    def session_params
+      params.permit(:email, :password)
+    end
 end
