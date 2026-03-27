63f539d93871fc2a6030e8d204bb5bd0eb1d9f15
ABaulderstone <alex.baulderstone@gmail.com>
2026-03-27 22:52:13 +1030

    adds model validation tests for slug_histories

--- backend/app/models/slug_history.rb
+++ backend/app/models/slug_history.rb
@@ -1,4 +1,6 @@
 class SlugHistory < ApplicationRecord
   belongs_to :user
   scope :active, -> { where("expires_at > ?", Time.current) }
+
+  validates :slug, :expires_at, presence: :true
 end
--- backend/spec/models/department_spec.rb
+++ backend/spec/models/department_spec.rb
@@ -11,6 +11,5 @@ RSpec.describe Department, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
-    it { should validate_inclusion_of(:billable).in_array([true, false]) }
   end
 end
